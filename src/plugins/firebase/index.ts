458e6bc8449ef3014c50f641adedc94b0cc42bae
Paulo Marques <paulo.marques@intelligenti.com.br>
2024-10-24 09:19:23 -0300

    Fix: .env changes

--- src/plugins/firebase/index.ts
+++ src/plugins/firebase/index.ts
@@ -4,13 +4,13 @@ import { initializeApp } from 'firebase/app'
 import type { App } from 'vue'
 
 const firebaseApp = initializeApp({
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 })
 
 export default function (app: App) {
